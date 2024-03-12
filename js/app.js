d8e1e77414cda2919a1f0b71f6083dc60045ec3d
JackFranks23 <Jack.franks23@student.cart.org>
2024-03-12 13:31:37 -0700

    Final Final commit

--- js/app.js
+++ js/app.js
@@ -14,15 +14,16 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("medicare").innerText = ((Math.round(grossmonthly*100*.0145)/100));
         document.getElementById("retirement").innerText = ((Math.round(grossmonthly*100*.05)/100));
         document.getElementById("insurance").innerText = (180);
-        // document.getElementById("netmonthly").innerText = (grossmonthlygrossmonthly-(Math.round(grossmonthly*100*.12)/100)-(Math.round(grossmonthly*100*.07)/100)-(Math.round(grossmonthly*100*.062)/100)-(Math.round(grossmonthly*100*.0145)/100)-(Math.round(grossmonthly*100*.05)/100)-180); //use the round here
+
 
         netMonthly = ((Math.round(grossmonthly*100*.3165)/100 - 180));
-        console.log(netMonthly)
         document.getElementById("netmonthly").innerText = netMonthly;
         document.getElementById("netmonthly2").innerText = netMonthly;
         document.getElementById("netmonthly3").innerText = netMonthly;
 
+        // page 1 up page 2 down
 
+        //left column
         document.getElementById("savings").innerText = ((Math.round(netMonthly*100*.10)/100));
         document.getElementById("housing").innerText = ((Math.round(netMonthly*100*.20)/100));
         document.getElementById("utilities").innerText = ((Math.round(netMonthly*100*.05)/100));
@@ -33,20 +34,20 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("personal").innerText = ((Math.round(netMonthly*100*.10)/100));
         document.getElementById("remainder").innerText = ((Math.round(netMonthly*100*.10)/100));
         
-        
+        //middle column
         let chartsavings = document.getElementById('Psavings').value;
         let charthousing = document.getElementById('Phousing').value;
-        let chartutilties = document.getElementById('Putilities').value;
+        let chartutilities = document.getElementById('Putilities').value;
         let charttransportation = document.getElementById('Ptransportation').value;
         let chartstudentloans = document.getElementById('Pstudentloans').value;
         let chartfood = document.getElementById('Pfood').value;
         let chartclothing = document.getElementById('Pclothing').value;
         let chartpersonal = document.getElementById('Ppersonal').value;
 
-
+        //right column
         document.getElementById("Piesavings").innerText = (((Math.round((chartsavings / netMonthly)*100.00)) + '%'));
         document.getElementById("Piehousing").innerText = (((Math.round((charthousing / netMonthly)*100.00)) + '%'));
-        document.getElementById("Pieutilties").innerText = (((Math.round((chartutilties / netMonthly)*100.00)) + '%'));
+        document.getElementById("Pieutilties").innerText = (((Math.round((chartutilities / netMonthly)*100.00)) + '%'));
         document.getElementById("Pietransportation").innerText = (((Math.round((charttransportation / netMonthly)*100.00)) + '%'));
         document.getElementById("Piestudentloans").innerText = (((Math.round((chartstudentloans / netMonthly)*100.00)) + '%'));
         document.getElementById("Piefood").innerText = (((Math.round((chartfood / netMonthly)*100.00)) + '%'));
@@ -54,14 +55,11 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("Piepersonal").innerText = (((Math.round((chartpersonal / netMonthly)*100.00)) + '%'));
 
 
-        let chartremainder = ((Math.round((netMonthly - chartsavings - charthousing - chartutilties - charttransportation - chartstudentloans - chartfood - chartclothing - chartpersonal)*100)/100));
+        let chartremainder = ((Math.round((netMonthly - chartsavings - charthousing - chartutilities - charttransportation - chartstudentloans - chartfood - chartclothing - chartpersonal)*100)/100));
 
         document.getElementById("Premainder").innerText = chartremainder
         document.getElementById("Pieremainder").innerText = (((Math.round((chartremainder / netMonthly)*100.00)) + '%'));
         
-        // document.getElementById('Premainder').innertext = (netMonthly - chartsavings - charthousing - chartutilities - chartransportation - chartstudentloans - chartfood - chartcothing - chartpersonal);
-
-
 
     });
 
